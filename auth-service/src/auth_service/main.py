aa419e8083d52a2f06e2cb7c25dff56739765a7e
himansu9805 <himansu9805@gmail.com>
2024-12-27 23:28:32 +0530

    Refactor import order and change host in main function

--- auth-service/src/auth_service/main.py
+++ auth-service/src/auth_service/main.py
@@ -1,11 +1,10 @@
 """This is the main module of the auth service."""
-from fastapi.responses import RedirectResponse
-import uvicorn
-from fastapi import FastAPI
 
+import uvicorn
 from auth_service import const
 from auth_service.routes.users import user_router
-
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 api = FastAPI(
     title="Auth Service API",
@@ -30,7 +29,7 @@ async def health():
 
 def main():
     """This is the main function of the auth service."""
-    uvicorn.run(api, host="0.0.0.0", port=8000)
+    uvicorn.run(api, host="127.0.0.1", port=8000)
 
 
 if __name__ == "__main__":

0bf00198e8f3a53c82f2af4f51fe9008fcb04132
himansu9805 <himansu9805@gmail.com>
2025-01-02 21:33:31 +0530

    Add CORS middleware in auth service

--- auth-service/src/auth_service/main.py
+++ auth-service/src/auth_service/main.py
@@ -4,12 +4,26 @@ import uvicorn
 from auth_service import const
 from auth_service.routes.users import user_router
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
 api = FastAPI(
     title="Auth Service API",
     description="This is the API for the Auth Service.",
     version=const.API_VERSION,
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 api.include_router(user_router, prefix=f"/api/{const.API_VERSION}")
